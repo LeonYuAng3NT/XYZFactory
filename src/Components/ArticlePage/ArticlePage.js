d5030562f5aec4cd45c9bc8f3281b770dc29a466
LeonYuAng3NT <leonzhang1996@hotmail.com>
2019-07-04 13:08:52 -0400

    add all changes along with new graphs for article

--- src/Components/ArticlePage/ArticlePage.js
+++ src/Components/ArticlePage/ArticlePage.js
@@ -1,7 +1,10 @@
 import _ from 'lodash'
 import React, { Component } from 'react'
 import processors from './processors.png'
+import processors2 from './processors2.png'
 import graph from './graph.png'
+import graph1 from './graph1.png'
+import graph2 from './graph2.png'
 import logo from './logo.png'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ContactPage from '../ContactPage/ContactPage'
@@ -11,6 +14,7 @@ import {
     Button,
     Container,
     Divider,
+    Item,
     Dropdown,
     Grid,
     Header,
@@ -60,21 +64,46 @@ const fixedOverlayMenuStyle = {
     left: '800px',
 }
 
-const LeftImage = () => (
-    <Image
-        floated='left'
-        size='medium'
-        src={processors} alt="processors"
-        style={{ margin: '2em 2em 2em -4em' }}
-    />
-)
-
 const RightImage = () => (
     <Image
         floated='right'
         size='medium'
         src={graph} alt="graph"
       style={{ margin: '2em -4em 2em 2em' }}
+    />
+
+)
+const NameImage = () => (
+    <Image
+        floated='left'
+        size='medium'
+        src={processors} alt="processors"
+       
+    /> 
+)
+const SecondRightImage = () => (
+    <Image
+        floated='right'
+        size='medium'
+        src={graph1} alt="graph1"
+        style={{ margin: '2em -4em 2em 2em' }}
+    />
+)
+const SecondGraphImage = () => (
+    <Image
+        floated='right'
+        size='medium'
+        src={graph2} alt="graph2"
+        style={{ margin: '2em -4em 2em 2em' }}
+    />
+)
+
+const SecondProcessorsImage = () => (
+    <Image
+        floated='right'
+        size='medium'
+        src={processors2} alt="processors2"
+        style={{ margin: '2em -4em 2em 2em' }}
     />
 )
 
@@ -92,25 +121,87 @@ const Paragraph = () => (
         ].join('')}
     </p>
 )
+const typelist = ['Ryzen 5 2600 @4.0GHz',
+    '2x 4GB XPG DDR4 2666 16 - 16 - 16 - 39',
+    'Asrock x370 Taichi',
+    'intel 760p 512GB SSD',
+    'MSI V1 Radeon RX 580 8GB',
+    'Corsair HX 1200 Platinum']
+const listItems = (list) => list.map((item) =>
+    <li>{item}</li>
+);
 const SecondParagraph = () => (
     <p>
         {[
-            'The main purpose of this is to test the bandwidth available for this operation and compare various memory configurations as well as various systems. For our initial testing, the benchmark will run on following systems to attain the results for the comparison:\r\nRyzen 5 2600 @4.0GHz, dual channel 8GB DDR4 @2666MHz 16-16-16-39\r\nRyzen 5 2600 @4.0GHz, Single channel 8GB DDR4 @2666MHz 16-16-16-39\r\nRyzen 7 1700 @3.8GHz, dual channel 8GB DDR4 @2666MHz 16-16-16-39\r\nIntel i7-3632-QM, dual channel 8GB DDR3 @1666MHz 11-11-11-28\r\nIntel i7-3632-QM, Single channel 8GB DDR3 @1666MHz 11-11-11-28\r\nintel i7-7700K @4.2GHz, Dual channel 32GB DDR4 @3000MHz 15-17-17-35\r\nSnapdragon 845 SoC (Using Samsung S9+ with Termux)\r\nAMD Opteron 6348 x4, quad channel 64GB DDR3\r\n',
-            'The following chart shows the results of the benchmarks:\r\n\r\n\r\n\r\n\r\n\r\nIn terms of the two options to write to memory, there was not much significant differences, as the outcome is mainly dependent on the system that the program runs on.\r\n\r\n\r\nAs expected, we see single channel mode to be on the bottom. But what is more interesting is that under this method of writing to memory, intel does not scale with more threads whatsoever and the bandwidth only decrease as the number of threads increase which is potentially due to contention of resources and extra instructions.',
-            'Surprisingly, the Snapdragon system actually beats AMD for single threaded as well as the absolute maximum while the bandwidth slightly increases as the number of threads increase. As for our Ryzen system, the bandwidth appears to be maxed out at 2 threads and decreases as the number of threads increase. ',
-             'Lastly for our Opteron, we would give it an E for effort as it does scale from an increase in threads, it does not have a high throughput at all compared to the others.',
-        ].join('')}
-    </p>
+            'The main purpose of this is to test the bandwidth available for this operation and compare various memory configurations as well as various systems. For our initial testing, the benchmark will run on following systems to attain the results for the comparison:'].join('')}
+            </p>
+   
+    
+    
 )
 
 const ThirdParagraph = () => (
     <p>
         {[
-            '\r\nMemory Overclocks\r\nAs Ryzen is known to scale with memory overclocks we will proceed with our testing on our Ryzen systems. Our system for this test will be the 2600, as it had a higher maximum bandwidth at our initial test and \r\nwhy 2600 over the 1700?\r\nAlthough we do not know why the bandwidth scales\r\n The system for the test will be built with the following:\r\nRyzen 5 2600 @4.0GHz\r\n2x 4GB XPG DDR4 2666 16-16-16-39\r\nAsrock x370 Taichi\r\nintel 760p 512GB SSD\r\nMSI V1 Radeon RX 580 8GB\r\nCorsair HX 1200 Platinum\r\n\r\nTo start off, we begin with once again using the XMP profile of 2666MHz 16-16-16-39 for our baseline and the timings of 17-17-17-36 for our testings with variations in memory clocks as this is the lowest possible when the clock speed is maximized to 3000MHz. \r\n<chart>\r\nHere we can see that the bandwidth maximizes to 16.6 GB\/s with 3000MHz 17-17-17-36 and the bandwidth decreases as the memory frequency decrease.\r\n\r\nNext, we want to see what about the timings, what is the effect of having tighter timings while decreasing the clocked speed of the memory\r\n\r\nAs an added bonus for our final test to see if CPU clock speed affects memory, we ran the test wtih our 2600 at stock, 3.0GHz, and 2.5GHz.\r\n\r\nThere was not very much significant changes past 2 threads on my ryzen 2600 system with the maximum at when writing at 2 threads. The bottleneck appears to be due to dual channel.\r\nThis can be further confirmed when the memory is physically removed leaving 1 stick\r\n\r\n\r\nProgramming conclusion:\r\nMobile, supported instructions\r\noptimizations for intel\r\noptimizations for AMD\r\n\r\n\r\nOverclocking conclusion:\r\nIn general, while many common workloads do not depend on memory or use these type of instructions for memory, it still may be worth while to take advantage of the increase in performance\r\nIn terms of ryzen, as noted that this platform is dependent on memory it may be best to increase the clock speed while loosening the timings to get a more well rounded system for various workloads. As we can see, the timings did not have as much of a significant impact as the clock speed.\r\n\r\n',
+            'The following chart shows the results of the benchmarks:\r\n\r\n\r\n\r\n\r\n\r\nIn terms of the two options to write to memory, there was not much significant differences, as the outcome is mainly dependent on the system that the program runs on.\r\n\r\n\r\n\r\n\r\n\r\nAs expected, we see single channel mode to be on the bottom. But what is more interesting is that under this method of writing to memory, intel does not scale with more threads whatsoever and the bandwidth only decrease as the number of threads increase which is potentially due to contention of resources and extra instructions.',
+            'Surprisingly, the Snapdragon system actually beats AMD for single threaded as well as the absolute maximum while the bandwidth slightly increases as the number of threads increase. As for our Ryzen system, the bandwidth appears to be maxed out at 2 threads and decreases as the number of threads increase. ',
+            'Lastly for our Opteron, we would give it an E for effort as it does scale from an increase in threads, it does not have a high throughput at all compared to the others.',
+
+        ].join('')}
+    </p>,
+    <p>
+        {[
+            'As Ryzen is known to scale with memory overclocks we will proceed with our testing on our Ryzen systems. Our system for this test will be the 2600, as it had a higher maximum bandwidth at our initial test and \r\nwhy 2600 over the 1700?\r\nAlthough we do not know why the bandwidth scales\r\n ',
+             'The system for the test will be built with the following:',
+          
         ].join('')}
     </p>
+
 )
 
+
+const FourthParagraph = () => (
+    <p>
+        {[
+            'In both methods of writing to memory, we see that when running in single channel, the maximum bandwidth is attained by using only one thread. This is also significantly lower when compared to any of the dual channel modes except for dual channel at 1866MHz. ',
+'Next, consider the bandwidth of dual channel.In all cases, it appears that the bandwidth is maximized with two active threads running the algorithm. At each clock speed that was tested, we see that the bandwidth can be further maximized by tightening the timing. With the maximum bandwidth of 16.6GB/s with 3000MHz at 17-17-17-36, it appears that, when overclocking memory, the best case seems to be to prioritize the maximization of the frequency of the memory first before tightening up any timings.',
+        'As an added bonus for our final test to bring some closure to the insignificance of using Ryzen 7 1700 vs Ryzen 5 2600, we ran the test on our Ryzen 7 1700 at 1.6GHz to see if the older CPU with a lower clock speed affects memory. Please take a look at the following output:',
+        
+        'Therefore, we can see that the frequency of the CPU and an older architecture did not have any significance on this test as we were able to reach a bandwidth that is with minimal differences as the Ryzen 2600 running at 4GHz.',
+        
+        ].join('')}
+    </p>
+
+)
+
+
+
+const FifthParagraph = () => (
+    <p>
+        {[
+            '\r\n\r\n\r\n\r\n\r\n\r\nProgramming conclusion:',
+'\r\n\r\n\r\n\r\n\r\n\r\nFrom this test, it may appear that when writing programs that involves significant memory operations, it may be helpful to consider the following.',
+'For mobile applications, the bandwidth scales well with an increase in the number of threads actively writing to memory.So to optimize mobile applications, one may consider liberally using all the cores available to their disposal.',
+        'Applications written for the intel platform may consider only utilizing one thread, unless the parallized nature of the application have other requirements.When writing with only one thread the application can leave resources to other parts of the application or other applications for a more overall optimal use of resources.',
+        'Optimizations for AMD on the other hand may be best to write in parallel.If the program can be written to utilize multiple threads, one may consider only using two threads active at a time for memory operations.We saw evidence of this with the bandwidth being maximized at two threads and slowly decreases as the number of threads increases.',
+        
+        '\r\n\r\n\r\n\r\n\r\n\r\nOverclocking conclusion:',
+        '\r\n\r\n\r\n\r\n\r\n\r\nIn general, while many common workloads do not depend on memory or use these type of instructions for memory, it still may be worthwhile to take advantage of the increase in performance.Personally, from testing on the Ryzen platform, I have a preference on maximizing the frequency of the memory over tightening the timings of memory.Especially since Ryzen is dependent on memory it may be better  to increase the clock speed while loosening the timings to get a more well rounded system for various workloads.' ,
+        'If one would like to make use of this tool, feel free to clone it from the GitHub repo and run it to optimize their memory overclock.To run the application after cloning the repo please refer to the README document.',
+        
+                ].join('')}
+    </p>
+
+)
+function DisplayList(list) {
+    return (
+        <div>
+            {list.map((item, index) => (
+                <Item key={index} item={item} />
+            ))}
+        </div>
+    );
+}
 export default class StickyLayout extends Component {
     ToContact() {
         this.setState({
@@ -134,6 +225,21 @@ export default class StickyLayout extends Component {
             overlayFixed: false,
 
         };
+        this.secondlist = ['Ryzen 5 2600 @4.0GHz',
+        '2x 4GB XPG DDR4 2666 16 - 16 - 16 - 39',
+        'Asrock x370 Taichi',
+        'intel 760p 512GB SSD',
+        'MSI V1 Radeon RX 580 8GB',
+        'Corsair HX 1200 Platinum']
+        this.firstlist = [
+        'Ryzen 5 2600 @4.0GHz, Single channel 8GB DDR4 @2666MHz 16 - 16 - 16 - 39',
+        'Ryzen 7 1700 @3.8GHz, dual channel 8GB DDR4 @2666MHz 16 - 16 - 16 - 39',
+        'Intel i7 - 3632QM, dual channel 8GB DDR3 @1666MHz 11 - 11 - 11 - 28',
+        'Intel i7 - 3632QM, Single channel 8GB DDR3 @1666MHz 11 - 11 - 11 - 28',
+        'Intel i7 - 7700K @4.2GHz, Dual channel 32GB DDR4 @3000MHz 15 - 17 - 17 - 35',
+        'Snapdragon 845 SoC(Using Samsung S9 + with Termux)',
+        'AMD Opteron 6348 x4, quad channel 64GB DDR3',
+]
         this.ToMain = this.ToMain.bind(this);
         this.ToContact = this.ToContact.bind(this);
     }
@@ -270,12 +376,21 @@ export default class StickyLayout extends Component {
                     
                     />
                     <RightImage />
-
-     
+                   
                     <SecondParagraph />
-                    <LeftImage />
+                   
 
+                    <ul>{listItems(this.firstlist)}</ul>
+                    <SecondGraphImage />
+                    <NameImage />
                     <ThirdParagraph />
+                    <SecondRightImage />
+                    <ul>{listItems(this.secondlist)}</ul>
+              
+                    <FourthParagraph />
+                    <SecondProcessorsImage/>
+                    <FifthParagraph/>
+                   
                 
 
                     <Grid.Row>
@@ -285,64 +400,7 @@ export default class StickyLayout extends Component {
                     </Grid.Row>
                 </Container>
 
-                <Segment inverted style={{ margin: '5em 0em 0em', padding: '5em 0em' }} vertical>
-                    <Container textAlign='center'>
-                        <Grid columns={4} divided stackable inverted>
-                            <Grid.Row>
-                                <Grid.Column>
-                                    <Header inverted as='h4' content='Group 1' />
-                                    <List link inverted>
-                                        <List.Item as='a'>Link One</List.Item>
-                                        <List.Item as='a'>Link Two</List.Item>
-                                        <List.Item as='a'>Link Three</List.Item>
-                                        <List.Item as='a'>Link Four</List.Item>
-                                    </List>
-                                </Grid.Column>
-                                <Grid.Column>
-                                    <Header inverted as='h4' content='Group 2' />
-                                    <List link inverted>
-                                        <List.Item as='a'>Link One</List.Item>
-                                        <List.Item as='a'>Link Two</List.Item>
-                                        <List.Item as='a'>Link Three</List.Item>
-                                        <List.Item as='a'>Link Four</List.Item>
-                                    </List>
-                                </Grid.Column>
-                                <Grid.Column>
-                                    <Header inverted as='h4' content='Group 3' />
-                                    <List link inverted>
-                                        <List.Item as='a'>Link One</List.Item>
-                                        <List.Item as='a'>Link Two</List.Item>
-                                        <List.Item as='a'>Link Three</List.Item>
-                                        <List.Item as='a'>Link Four</List.Item>
-                                    </List>
-                                </Grid.Column>
-                                <Grid.Column>
-                                    <Header inverted as='h4' content='Footer Header' />
-                                    <p>
-                                        Extra space for a call to action inside the footer that could help re-engage
-                                        users.
-                  </p>
-                                </Grid.Column>
-                            </Grid.Row>
-                        </Grid>
-                        <Divider inverted section />
-                        <Image src='/logo.png' centered size='mini' />
-                        <List horizontal inverted divided link size='small'>
-                            <List.Item as='a' href='#'>
-                                Site Map
-              </List.Item>
-                            <List.Item as='a' href='#'>
-                                Contact Us
-              </List.Item>
-                            <List.Item as='a' href='#'>
-                                Terms and Conditions
-              </List.Item>
-                            <List.Item as='a' href='#'>
-                                Privacy Policy
-              </List.Item>
-                        </List>
-                    </Container>
-                </Segment>
+            
             </div>);
         if (this.state.ToContact === true) {
             view = (
